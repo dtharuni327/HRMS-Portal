6d61ae9c6626cff6c7bd346c134b03babeb80cff
Kiruthika-1810 <kiruthi508@gmail.com>
2026-04-24 15:23:12 +0530

    Added login & register API

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,17 @@ import userRoutes from "./routes/user.routes";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); 
 
-app.use("/api/users", userRoutes);
-
-app.get("/",(req,res)=>{
-    res.send("API Working");
-});
+app.use("/api/users", userRoutes); 
 
 export default app;
+
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
+app.use((req, res, next) => {
+  console.log("🔥 Incoming:", req.method, req.url);
+  next();
+});
