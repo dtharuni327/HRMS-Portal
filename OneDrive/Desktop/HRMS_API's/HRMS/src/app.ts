aadaac390fede11b3609be2228096632ecf9c6cf
Kiruthika-1810 <kiruthi508@gmail.com>
2026-06-16 12:17:45 +0530

    Added leave type management

--- OneDrive/Desktop/HRMS_API's/HRMS/src/app.ts
+++ OneDrive/Desktop/HRMS_API's/HRMS/src/app.ts
@@ -1,30 +1,47 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import authenticationRoutes from "./routes/authentication.routes";
 import holidayRoutes from "./routes/holiday.routes";
 import leaveRoutes from "./routes/leave.routes";
+
 dotenv.config();
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-app.get("/",
-  (req,res) => {
-    res.status(200).json({
-      success: true,
-      message: "HRMS API Running Successfully"
-    });
-  }
-);
-app.use("/auth",authenticationRoutes);
-app.use("/holiday",holidayRoutes);
-app.use("/leave",leaveRoutes);
 app.use(
-  (error: any,req: any,res: any,next: any) => {
+  express.urlencoded({
+    extended: true
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "HRMS API Running Successfully"
+  });
+});
+
+/* Routes */
+
+app.use("/auth", authenticationRoutes);
+app.use("/holiday", holidayRoutes);
+app.use("/leave", leaveRoutes);
+
+/* Global Error Handler */
+
+app.use(
+  (
+    error: any,
+    req: any,
+    res: any,
+    next: any
+  ) => {
     console.error(error);
+
     res.status(
       error.statusCode || 500
     ).json({
@@ -35,4 +52,5 @@ app.use(
     });
   }
 );
+
 export default app;
--- OneDrive/Desktop/HRMS_API's/HRMS/src/storedProcedures/Holiday/USP_Get_Total_Holidays.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE USP_Get_Total_Holidays
-AS
-BEGIN
-    SELECT
-        COUNT(*) AS TotalHolidays
-    FROM Holidays
-    WHERE YEAR(HolidayDate) = YEAR(GETDATE());
-END
-GO
